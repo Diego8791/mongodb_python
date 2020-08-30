bc3878d582f67cecf99a8bcc745d4aca41bbe95f
Diego8791 <dfarias8791@gmail.com>
2020-08-29 22:08:59 -0300

    Ejercicios Clase
    Completo

--- ejercicios_clase.py
+++ ejercicios_clase.py
@@ -52,6 +52,23 @@ def fill():
 
     # Se debe utilizar la sentencia insert_one o insert_many.
 
+    conn = TinyMongoClient()
+    db = conn[db_name]
+
+    students = [{"name": "Luciana", "age": 17, "grade": 4, "tutor": "Mariela"},
+                {"name": "Joaquin", "age": 16, "grade": 3, "tutor": "Mariela"},
+                {"name": "Martina", "age": 17, "grade": 4, "tutor": "Candela"},
+                {"name": "Carolina", "age": 17, "grade": 4, "tutor": "Candela"},
+                {"name": "Jose", "age": 15, "grade": 3, "tutor": "Mariela"},
+                {"name": "Ricardo", "age": 16, "grade": 3, "tutor": "Candela"},
+                {"name": "Santiago", "age": 17, "grade": 4, "tutor": "Mariela"},
+                {"name": "Paola", "age": 16, "grade": 3, "tutor": "Candela"},
+                {"name": "Mauro", "age": 15, "grade": 3, "tutor": "Candela"},
+                {"name": "Miguel", "age": 13, "grade": 2, "tutor": "Mariela"}]
+    
+    db.secundaria.insert_many(students)
+    conn.close()
+
 
 def show():
     print('Comprovemos su contenido, ¿qué hay en la tabla?')
@@ -60,15 +77,38 @@ def show():
     # Queda a su criterio serializar o no el JSON "dumps"
     #  para imprimirlo en un formato más "agradable"
 
+    conn = TinyMongoClient()
+    db = conn[db_name]
 
-def find_by_grade(grade):
+    cursor = db.secundaria.find()
+    data = list(cursor)
+    json_data = json.dumps(data, indent=4)
+    print(json_data)
+
+    conn.close()
+    
+    
+def find_by_grade(grade):    # grade 3
     print('Operación búsqueda!')
     # Utilizar la sentencia find para imprimir en pantalla
     # aquellos estudiantes que se encuentra en en año "grade"
 
+    conn = TinyMongoClient()
+    db = conn[db_name]
+
+    cursor = db.secundaria.find({"grade": 3})
+    data = list(cursor)
+    json_data = json.dumps(data, indent=4)
+    print(json_data)
+
     # De la lista de esos estudiantes debe imprimir
     # en pantalla unicamente los siguiente campos por cada uno:
     # id / name / age
+
+    for dato in cursor:
+        print('id: {}, name: {}, age: {}'.format(dato["_id"], dato["name"], dato["age"]))
+
+    conn.close()
 
 
 def insert(student):
@@ -78,17 +118,43 @@ def insert(student):
 
     # El parámetro student deberá ser un JSON el cual se inserta en la db
 
+    conn = TinyMongoClient()
+    db = conn[db_name]
+    
+    # insertar nuevo estudiante
+    db.secundaria.insert_one(student)
+    
+    # Imprimir la base de datos actualizada
+    cursor = db.secundaria.find()
+    data = list(cursor)
+    json_data = json.dumps(data, indent=4)
+    print(json_data)
+    
+    conn.close()
+
 
 def count(grade):
     print('Contar estudiantes')
     # Utilizar la sentencia find + count para contar
     # cuantos estudiantes pertenecen el grado "grade"
 
+    conn = TinyMongoClient()
+    db = conn[db_name]
+
+    filtro_grado = db.secundaria.find({"grade": grade})
+    for dato in filtro_grado:
+        print(dato)
+    
+    count = db.secundaria.find({"grade": grade}).count()
+    print('La cantidad de estudiantes en', grade, 'grado son', count)
+    
+    conn.close()
+
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
     # Borrar la db
-    clear()
+    # clear()
 
     # fill()
     # show()
@@ -96,7 +162,7 @@ if __name__ == '__main__':
     grade = 3
     # find_by_grade(grade)
 
-    # student = {....}
+    student = {"name": "Mariano", "age": 18, "grade": 6, "tutor": "Mariela"}
     # insert(student)
 
-    # count(grade)
+    count(grade)
